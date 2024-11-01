122cdad3a0d5b0888aeb3c0ef1572c5d5c5a25fe
13ruceYu <bruceyuhongbo@gmail.com>
2024-11-01 16:57:15 +0800

    feat: format with astro and sperate theme toggle

--- src/consts.ts
+++ src/consts.ts
@@ -1,36 +1,36 @@
-import type { Site, Metadata, Socials } from "@types";
+import type { Site, Metadata, Socials } from '@types'
 
 export const SITE: Site = {
-  NAME: "Bruce's Blog",
-  EMAIL: "bruceyuhongbo@gmail.com",
+  NAME: 'Bruce\'s Blog',
+  EMAIL: 'bruceyuhongbo@gmail.com',
   NUM_POSTS_ON_HOMEPAGE: 10,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
-};
+}
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Bruce's Portfolio",
-};
+  TITLE: 'Home',
+  DESCRIPTION: 'Bruce\'s Portfolio',
+}
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
-};
+  TITLE: 'Blog',
+  DESCRIPTION: 'A collection of articles on topics I am passionate about.',
+}
 
 export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
-};
+  TITLE: 'Work',
+  DESCRIPTION: 'Where I have worked and what I have done.',
+}
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
-};
+  TITLE: 'Projects',
+  DESCRIPTION: 'A collection of my projects, with links to repositories and demos.',
+}
 
 export const SOCIALS: Socials = [
   { 
-    NAME: "GitHub",
-    HREF: "https://github.com/13ruceYu"
+    NAME: 'GitHub',
+    HREF: 'https://github.com/13ruceYu'
   },
-];
+]
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,27 +1,27 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
     draft: z.boolean().optional()
   }),
-});
+})
 
 const work = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     company: z.string(),
     role: z.string(),
     dateStart: z.coerce.date(),
     dateEnd: z.union([z.coerce.date(), z.string()]),
   }),
-});
+})
 
 const projects = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -30,6 +30,6 @@ const projects = defineCollection({
     demoURL: z.string().optional(),
     repoURL: z.string().optional()
   }),
-});
+})
 
-export const collections = { blog, work, projects };
+export const collections = { blog, work, projects }
