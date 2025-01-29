8dd9172b4a57d349af5dea4d5e6df43fa02194fd
13ruceYu <bruceyuhongbo@gmail.com>
2025-01-29 11:37:24 +0800

    chore: file delete and mobile size optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 
 export default defineConfig({
-  site: "https://bruceyu.fun",
+  site: 'https://bruceyu.fun',
   integrations: [mdx(), sitemap(), tailwind(), icon()],
-});
+})
